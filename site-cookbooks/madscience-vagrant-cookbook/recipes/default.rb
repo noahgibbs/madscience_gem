60c4dae859ef4356566d0e342758c24e95c11687
Noah Gibbs <noah_gibbs@yahoo.com>
2014-07-30 05:53:13 -0700

    Debug debug debug

--- site-cookbooks/madscience-vagrant-cookbook/recipes/default.rb
+++ site-cookbooks/madscience-vagrant-cookbook/recipes/default.rb
@@ -10,45 +10,44 @@
 # Default attributes in a cookbook override default
 # attributes in an attributes file, like the one
 # in vagrant-cookbook/attributes/default.rb.
-default['vagrant']['plugins'] = [ 'vagrant-omnibus' ]
+node.default['vagrant']['plugins'] = [ 'vagrant-omnibus' ]
 
 case node['platform_family']
-  when 'fedora'
-    if node['kernel']['machine'] == 'x86_64'
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.rpm'
-    else
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.rpm'
-    end
-  when 'rhel'
-    if node['kernel']['machine'] == 'x86_64'
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.rpm'
-    else
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.rpm'
-    end
-  when 'debian'
-    if node['kernel']['machine'] == 'x86_64'
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb'
-    else
-      default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.deb'
-    end
-  when 'windows'
-    # TODO: find real MSI version
-    default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.msi'
-  when 'mac_os'
-    default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg'
+when 'fedora'
+  if node['kernel']['machine'] == 'x86_64'
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.rpm'
   else
-    raise "Don't recognize OS family: #{node['platform_family'].inspect}!"
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.rpm'
   end
+when 'rhel'
+  if node['kernel']['machine'] == 'x86_64'
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.rpm'
+  else
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.rpm'
+  end
+when 'debian'
+  if node['kernel']['machine'] == 'x86_64'
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb'
+  else
+    node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_32.deb'
+  end
+when 'windows'
+  # TODO: find real MSI version
+  node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.msi'
+when 'mac_os_x'
+  node.default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg'
+else
+  raise "Don't recognize OS family: #{node['platform_family'].inspect}!"
 end
 
 case node['platform_family']
 when 'mac_os_x'
-  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/4.3.12/VirtualBox-4.3.12-93733-OSX.dmg'
+  node.default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/4.3.12/VirtualBox-4.3.12-93733-OSX.dmg'
 when 'windows'
-  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/4.3.12/VirtualBox-4.3.12-93733-Win.exe'
-  default['virtualbox']['version'] = Vbox::Helpers.vbox_version(node['virtualbox']['url'])
+  node.default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/4.3.12/VirtualBox-4.3.12-93733-Win.exe'
+  node.default['virtualbox']['version'] = Vbox::Helpers.vbox_version(node['virtualbox']['url'])
 when 'debian', 'rhel'
-  default['virtualbox']['version'] = '4.3'
+  node.default['virtualbox']['version'] = '4.3'
 end
 
 
--- solo.rb
+++ solo.rb
@@ -1,3 +1,6 @@
-cookbook_path ["site-cookbooks", "cookbooks"]
+cb1 = File.expand_path "../site-cookbooks", __FILE__
+cb2 = File.expand_path "../cookbooks", __FILE__
+
+cookbook_path [cb1, cb2]
 json_attribs "node-data.json"
 ssl_verify_mode :verify_peer
