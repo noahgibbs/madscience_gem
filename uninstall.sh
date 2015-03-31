8979a3eca1da18f40fd446c6ff71919c94b10f18
Noah Gibbs <noah.gibbs@onlive.com>
2015-03-31 12:18:37 -0700

    Do unattended VirtualBox uninstall when requested

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,7 @@
 # Note: this uninstalls Vagrant, NOT the madscience gem.
 sudo rm -rf /usr/bin/vagrant /opt/vagrant /Applications/Vagrant
 sudo pkgutil --forget com.vagrant.vagrant
+
+# And this should uninstall VirtualBox. Script copied from
+# VirtualBox DMG uninstaller.
+./VirtualBox_Uninstall.tool --unattended
