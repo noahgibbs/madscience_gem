ade8d8753351c5e09a194f692908dc2f1cae4508
Noah Gibbs <noah.gibbs@onlive.com>
2015-04-11 15:18:38 -0700

    Remove Vagrant user plugins from normal and root accounts

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,10 @@
 sudo rm -rf /usr/bin/vagrant /opt/vagrant /Applications/Vagrant
 sudo pkgutil --forget com.vagrant.vagrant
 
+# Remove root's vagrant dir and current user's vagrant dir
+sudo rm -rf /var/root/.vagrant.d
+rm -rf ~/.vagrant.d
+
 # And this should uninstall VirtualBox. Script copied from
 # VirtualBox DMG uninstaller.
 ./VirtualBox_Uninstall.tool --unattended
